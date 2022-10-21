090d3a08ef8964d75dbb9cc1f8c92185521568b2
Nathan Pritchett <npritchett@hartfield.org>
2022-10-21 13:19:25 -0500

    Basic implement of swivel and lift into autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -13,15 +13,26 @@ public class Auto extends LinearOpMode {
     private DcMotor motor_drive_rf;
     private DcMotor motor_drive_lr;
     private DcMotor motor_drive_rr;
+    private DcMotor motor_lift;
+    private DcMotor motor_swivel;
 
     private ElapsedTime runtime = new ElapsedTime();
 
-    static final double     COUNTS_PER_MOTOR_REV    = 537.7 ;    // eg: TETRIX Motor Encoder
+    //Swivel and lift use arbitrary numbers since they aren't wheels. For instance, 500 on the swivel would not be 500mm, just a set encoder distance.
+    //There's probably a better way to do it but this works for now.
+    static final double     COUNTS_PER_MOTOR_GOBUILDA    = 537.7 ;    // eg: TETRIX Motor Encoder
+    static final double     COUNTS_PER_MOTOR_SWIVEL    = 537.7 ;
     static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
+    static final double     SWIVEL_GEAR_REDUCTION    = 1.0 ;
     static final double     WHEEL_DIAMETER_MM   = 95.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_MM         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+    static final double     COUNTS_PER_MM_GOBUILDA         = (COUNTS_PER_MOTOR_GOBUILDA * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_MM * 3.1415);
+    static final double     COUNTS_PER_MM_SWIVEL         = (COUNTS_PER_MOTOR_SWIVEL * SWIVEL_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_MM * 3.1415);
     static final double     DRIVE_SPEED             = 0.6;
+    static final double     UP_LIFT_SPEED           = 0.5;
+    static final double     DOWN_LIFT_SPEED         = 0.3;
+    static final double     SWIVEL_SPEED            = 0.3;
     static final double     TURN_SPEED              = 0.5;
 
     /**
@@ -33,6 +44,8 @@ public class Auto extends LinearOpMode {
         motor_drive_rf = hardwareMap.get(DcMotor.class, "motor_drive_rf");
         motor_drive_lr = hardwareMap.get(DcMotor.class, "motor_drive_lr");
         motor_drive_rr = hardwareMap.get(DcMotor.class, "motor_drive_rr");
+        motor_lift = hardwareMap.get(DcMotor.class, "motor_lift");
+        motor_swivel = hardwareMap.get(DcMotor.class, "motor_swivel");
 
         // Reverse one of the drive motors.
         // You will have to determine which motor to reverse for your robot.
@@ -41,29 +54,39 @@ public class Auto extends LinearOpMode {
         motor_drive_rf.setDirection(DcMotorSimple.Direction.REVERSE);
         motor_drive_rr.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        motor_lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor_swivel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         motor_drive_lf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor_drive_rf.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor_drive_lr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor_drive_rr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//m
+        motor_lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor_swivel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         motor_drive_lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor_drive_rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor_drive_lr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor_drive_rr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor_lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor_swivel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Send telemetry message to indicate successful Encoder reset
         telemetry.addData("Starting at",  "%7d :%7d",
                 motor_drive_lf.getCurrentPosition(),
                 motor_drive_rf.getCurrentPosition(),
                 motor_drive_lr.getCurrentPosition(),
-                motor_drive_rr.getCurrentPosition());
+                motor_drive_rr.getCurrentPosition(),
+                motor_lift.getCurrentPosition(),
+                motor_swivel.getCurrentPosition());
+
         telemetry.update();
 
         waitForStart();
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  500,  500, 500, 500, 5.0);
+        encoderDrive(DRIVE_SPEED,  500,  500, 500, 500, 0, 0, 5.0);
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
@@ -80,31 +103,40 @@ public class Auto extends LinearOpMode {
          *  3) Driver stops the opmode running.
          */
         public void encoderDrive(double speed,
-        double lfMM, double rfMM, double lrMM, double rrMM,
+        double lfMM, double rfMM, double lrMM, double rrMM, double lftMM, double swvMM,
         double timeoutS) {
             int newlfTarget;
             int newrfTarget;
             int newlrTarget;
             int newrrTarget;
+            int newlftTarget;
+            int newswvTarget;
 
             // Ensure that the opmode is still active
             if (opModeIsActive()) {
 
                 // Determine new target position, and pass to motor controller
-                newlfTarget = motor_drive_lf.getCurrentPosition() + (int)(lfMM * COUNTS_PER_MM);
-                newrfTarget = motor_drive_rf.getCurrentPosition() + (int)(rfMM * COUNTS_PER_MM);
-                newlrTarget = motor_drive_lr.getCurrentPosition() + (int)(lrMM * COUNTS_PER_MM);
-                newrrTarget = motor_drive_rr.getCurrentPosition() + (int)(rrMM * COUNTS_PER_MM);
+                newlfTarget = motor_drive_lf.getCurrentPosition() + (int)(lfMM * COUNTS_PER_MM_GOBUILDA);
+                newrfTarget = motor_drive_rf.getCurrentPosition() + (int)(rfMM * COUNTS_PER_MM_GOBUILDA);
+                newlrTarget = motor_drive_lr.getCurrentPosition() + (int)(lrMM * COUNTS_PER_MM_GOBUILDA);
+                newrrTarget = motor_drive_rr.getCurrentPosition() + (int)(rrMM * COUNTS_PER_MM_GOBUILDA);
+                newlftTarget = motor_lift.getCurrentPosition() + (int)(lftMM * COUNTS_PER_MM_GOBUILDA);
+                newswvTarget = motor_swivel.getCurrentPosition() + (int)(swvMM * COUNTS_PER_MM_SWIVEL);
+
                 motor_drive_lf.setTargetPosition(newlfTarget);
                 motor_drive_rf.setTargetPosition(newrfTarget);
                 motor_drive_lr.setTargetPosition(newlrTarget);
                 motor_drive_rr.setTargetPosition(newrrTarget);
+                motor_lift.setTargetPosition(newlftTarget);
+                motor_swivel.setTargetPosition(newswvTarget);
 
                 // Turn On RUN_TO_POSITION
                 motor_drive_lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 motor_drive_rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 motor_drive_lr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 motor_drive_rr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                motor_lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                motor_swivel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
                 // reset the timeout time and start motion.
                 runtime.reset();
@@ -112,6 +144,8 @@ public class Auto extends LinearOpMode {
                 motor_drive_rf.setPower(Math.abs(speed));
                 motor_drive_lr.setPower(Math.abs(speed));
                 motor_drive_rr.setPower(Math.abs(speed));
+                motor_lift.setPower(Math.abs(speed));
+                motor_swivel.setPower(Math.abs(speed));
 
                 // keep looping while we are still active, and there is time left, and both motors are running.
                 // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
@@ -121,13 +155,13 @@ public class Auto extends LinearOpMode {
                 // onto the next step, use (isBusy() || isBusy()) in the loop test.
                 while (opModeIsActive() &&
                         (runtime.seconds() < timeoutS) &&
-                        (motor_drive_lf.isBusy() && motor_drive_rf.isBusy() && motor_drive_lr.isBusy() && motor_drive_rr.isBusy())) {
+                        (motor_drive_lf.isBusy() && motor_drive_rf.isBusy() && motor_drive_lr.isBusy() && motor_drive_rr.isBusy() && motor_lift.isBusy() && motor_swivel.isBusy())) {
 
                     // Display it for the driver.
                     telemetry.addData("Running to",  " %7d :%7d", newlfTarget,  newrfTarget, newlrTarget, newrrTarget);
                     telemetry.addData("Currently at",  " at %7d :%7d",
-                            motor_drive_lf.getCurrentPosition(), motor_drive_rf.getCurrentPosition(), motor_drive_lr.getCurrentPosition(), motor_drive_rr.getCurrentPosition());
-                    telemetry.update();
+                            motor_drive_lf.getCurrentPosition(), motor_drive_rf.getCurrentPosition(), motor_drive_lr.getCurrentPosition(), motor_drive_rr.getCurrentPosition(),
+                    motor_lift.getCurrentPosition(), motor_swivel.getCurrentPosition(), telemetry.update());
                 }
 
                 // Stop all motion;
@@ -135,12 +169,16 @@ public class Auto extends LinearOpMode {
                 motor_drive_rf.setPower(0);
                 motor_drive_lr.setPower(0);
                 motor_drive_rr.setPower(0);
+                motor_lift.setPower(0);
+                motor_swivel.setPower(0);
 
                 // Turn off RUN_TO_POSITION
                 motor_drive_lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                 motor_drive_rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                 motor_drive_lr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                 motor_drive_rr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                motor_lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                motor_swivel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
                 sleep(250);   // optional pause after each move.
             }
