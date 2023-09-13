4b4d8f39a558074e1c29e0296811cb53fee501a2
Nathan Pritchett <npritchett@hartfield.org>
2023-09-13 10:48:30 -0500

    Remove references to Powerplay specific motors and servos from everything else

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWithObjectDetectionRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWithObjectDetectionRight.java
@@ -45,71 +45,20 @@ public class AutoWithObjectDetectionRight extends LinearOpMode
                                      V
          */
 
-        //Initial lift to be able to grab cone
-        encoderClass.encoderDrive(1, 0, 0, 0, 0, 50, 0, InitSetup.SERVO_CLOSED_AUTO, 5.0, this, initsetup);
-        //Drive forward to move signal
-        encoderClass.encoderDrive(0.55, 1500, 1500, 1500, 1500, 300, 0, InitSetup.SERVO_CLOSED_AUTO, 5.0, this, initsetup);
-        //Back up to free signal
-        encoderClass.encoderDrive(0.5, -250, -250, -250, -250, 0, 0, InitSetup.SERVO_CLOSED_AUTO, 5.0, this, initsetup);
-        //Strafe to the left and lift for initial score
-        encoderClass.encoderDrive(0.5, -350, 350, 350, -350, 800, 0, InitSetup.SERVO_CLOSED_AUTO, 3.5, this, initsetup);
-        //Forward for initial score
-        encoderClass.encoderDrive(0.4, 180, 180, 180, 180, 0, 0, InitSetup.SERVO_CLOSED_AUTO, 2.0, this, initsetup);
-        //Reset swivel position
-        initsetup.motor_swivel.setPower(-1);
-        sleep(100);
-        initsetup.motor_swivel.setPower(0);
-        //Slightly lower lift for more accurate scoring
-        encoderClass.encoderDrive(0.6, 0, 0, 0, 0, -100, 0, InitSetup.SERVO_CLOSED_AUTO, 2.0, this, initsetup);
-        //Drop cone
-        initsetup.claw_servo.setPosition(InitSetup.SERVO_OPEN_AUTO);
-        //Back up after initial score
-        encoderClass.encoderDrive(0.4, -180, -180, -180, -180, 0, 0, InitSetup.SERVO_OPEN_AUTO, 2.0, this, initsetup);
-        //Turn towards cones and drop lift part of the way (2)
-        encoderClass.encoderDrive(0.5, 460, -460, 460, -460, -440, 0, InitSetup.SERVO_CLOSED_AUTO, 5.0, this, initsetup);
-        //Reset swivel position
-        initsetup.motor_swivel.setPower(-1);
-        sleep(100);
-        initsetup.motor_swivel.setPower(0);
-        //Drive to cones and finish lowering arm (2)
-        encoderClass.encoderDrive(0.6, 1000, 1000, 1000, 1000, -300, 0, InitSetup.SERVO_OPEN_AUTO, 5.0, this, initsetup);
-        //Close claw (2)
-        initsetup.claw_servo.setPosition(InitSetup.SERVO_CLOSED_AUTO);
-        sleep(100);
-        //Lift cone (2)
-        encoderClass.encoderDrive(1, 0, 0, 0, 0, 300, 0, InitSetup.SERVO_CLOSED_AUTO, 2.0, this, initsetup);
-        //Back up to score (2)
-        encoderClass.encoderDrive(0.5, -980, -980, -980, -980, 0, 0, InitSetup.SERVO_CLOSED_AUTO, 5.0, this, initsetup);
-        //Turn towards junction and raise lift (2)
-        encoderClass.encoderDrive(0.5, -470, 470, -470, 470, 950, 0, InitSetup.SERVO_CLOSED_AUTO, 2.5, this, initsetup);
-        //Forward to score (2)
-        encoderClass.encoderDrive(0.4, 140, 140, 140, 140, 0, 0, InitSetup.SERVO_CLOSED_AUTO, 2.5, this, initsetup);
-        initsetup.motor_swivel.setPower(-1);
-        sleep(100);
-        initsetup.motor_swivel.setPower(0);
-        encoderClass.encoderDrive(0.6, 0, 0, 0, 0, -100, 0, InitSetup.SERVO_CLOSED_AUTO, 2.0, this, initsetup);
-        //Drop cone (2)
-        initsetup.claw_servo.setPosition(InitSetup.SERVO_OPEN_AUTO);
-        //Back up from score (2)
-        encoderClass.encoderDrive(0.4, -180, -180, -180, -180, 0, 0, InitSetup.SERVO_OPEN_AUTO, 5.0, this, initsetup);
 
         /* Actually do something useful */
         if(aprilTagsSetup.tagOfInterest == null){
             //default trajectory here if preferred
             //Park
-            encoderClass.encoderDrive(0.5, 360, -360, -360, 360, 0, 0, InitSetup.SERVO_CLOSED_AUTO, 5.0, this, initsetup);
         }else if(aprilTagsSetup.tagOfInterest.id == aprilTagsSetup.ONE){
             //Trajectory if tag one is detected
             //Park
-            encoderClass.encoderDrive(0.5, -360, 360, 360, -360, 0, 0, InitSetup.SERVO_CLOSED_AUTO, 5.0, this, initsetup);
         }else if(aprilTagsSetup.tagOfInterest.id == aprilTagsSetup.TWO){
             //Trajectory if tag two is detected
             //Park
-            encoderClass.encoderDrive(0.5, 360, -360, -360, 360, 0, 0, InitSetup.SERVO_CLOSED_AUTO, 5.0, this, initsetup);
         }else{
             //Trajectory if tag three is detected
             //Park
-            encoderClass.encoderDrive(0.5, 1060, -1060, -1060, 1060, 0, 0, InitSetup.SERVO_CLOSED_AUTO, 5.0, this, initsetup);
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderClass.java
@@ -10,14 +10,12 @@ public class EncoderClass {
     private final ElapsedTime runtime = new ElapsedTime();
 
     public void encoderDrive(double speed,
-                             double lfMM, double rfMM, double lrMM, double rrMM, double lftMM, double swvDG, double cwPos,
-                             double timeoutS, LinearOpMode opMode, InitSetup initpostsetup) {
+                             double lfMM, double rfMM, double lrMM, double rrMM, double timeoutS,
+                             LinearOpMode opMode, InitSetup initpostsetup) {
         int newlfTarget;
         int newrfTarget;
         int newlrTarget;
         int newrrTarget;
-        int newlftTarget;
-        int newswvTarget;
 
         // Ensure that the opmode is still active
         if (opMode.opModeIsActive()) {
@@ -27,23 +25,17 @@ public class EncoderClass {
             newrfTarget = initpostsetup.motor_drive_rf.getCurrentPosition() + (int)(rfMM * InitSetup.COUNTS_PER_MM_DRIVE);
             newlrTarget = initpostsetup.motor_drive_lr.getCurrentPosition() + (int)(lrMM * InitSetup.COUNTS_PER_MM_DRIVE);
             newrrTarget = initpostsetup.motor_drive_rr.getCurrentPosition() + (int)(rrMM * InitSetup.COUNTS_PER_MM_DRIVE);
-            newlftTarget = initpostsetup.motor_lift.getCurrentPosition() + (int)(lftMM * InitSetup.COUNTS_PER_MM_LIFT);
-            newswvTarget = initpostsetup.motor_swivel.getCurrentPosition() + (int)(swvDG * InitSetup.COUNTS_PER_DG_SWIVEL);
 
             initpostsetup.motor_drive_lf.setTargetPosition(newlfTarget);
             initpostsetup.motor_drive_rf.setTargetPosition(newrfTarget);
             initpostsetup.motor_drive_lr.setTargetPosition(newlrTarget);
             initpostsetup.motor_drive_rr.setTargetPosition(newrrTarget);
-            initpostsetup.motor_lift.setTargetPosition(newlftTarget);
-            initpostsetup.motor_swivel.setTargetPosition(newswvTarget);
 
             // Turn On RUN_TO_POSITION
             initpostsetup.motor_drive_lf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             initpostsetup.motor_drive_rf.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             initpostsetup.motor_drive_lr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             initpostsetup.motor_drive_rr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            initpostsetup.motor_lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            initpostsetup.motor_swivel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
             // reset the timeout time and start motion.
             runtime.reset();
@@ -51,92 +43,33 @@ public class EncoderClass {
             initpostsetup.motor_drive_rf.setPower(Math.abs(speed));
             initpostsetup.motor_drive_lr.setPower(Math.abs(speed));
             initpostsetup.motor_drive_rr.setPower(Math.abs(speed));
-            initpostsetup.motor_lift.setPower(Math.abs(speed));
-            initpostsetup.motor_swivel.setPower(Math.abs(speed));
-            initpostsetup.claw_servo.setPosition(cwPos);
 
             // keep looping while we are still active, and there is time left, and motors are not within the target threshold.
             while (opMode.opModeIsActive() &&
                     (runtime.seconds() < timeoutS) &&
-                    (Math.abs(initpostsetup.motor_drive_lf.getCurrentPosition()-initpostsetup.motor_drive_lf.getTargetPosition()) > InitSetup.ENCODER_TOLERANCE || Math.abs(initpostsetup.motor_drive_rf.getCurrentPosition()-initpostsetup.motor_drive_rf.getTargetPosition()) > InitSetup.ENCODER_TOLERANCE || Math.abs(initpostsetup.motor_drive_lr.getCurrentPosition()-initpostsetup.motor_drive_lr.getTargetPosition()) > InitSetup.ENCODER_TOLERANCE || Math.abs(initpostsetup.motor_drive_rr.getCurrentPosition()-initpostsetup.motor_drive_rr.getTargetPosition()) > InitSetup.ENCODER_TOLERANCE || Math.abs(initpostsetup.motor_lift.getCurrentPosition()-initpostsetup.motor_lift.getTargetPosition()) > InitSetup.ENCODER_TOLERANCE || Math.abs(initpostsetup.motor_swivel.getCurrentPosition()-initpostsetup.motor_swivel.getTargetPosition()) > InitSetup.ENCODER_TOLERANCE)) {
+                    (Math.abs(initpostsetup.motor_drive_lf.getCurrentPosition()-initpostsetup.motor_drive_lf.getTargetPosition()) > InitSetup.ENCODER_TOLERANCE || Math.abs(initpostsetup.motor_drive_rf.getCurrentPosition()-initpostsetup.motor_drive_rf.getTargetPosition()) > InitSetup.ENCODER_TOLERANCE || Math.abs(initpostsetup.motor_drive_lr.getCurrentPosition()-initpostsetup.motor_drive_lr.getTargetPosition()) > InitSetup.ENCODER_TOLERANCE || Math.abs(initpostsetup.motor_drive_rr.getCurrentPosition()-initpostsetup.motor_drive_rr.getTargetPosition()) > InitSetup.ENCODER_TOLERANCE)) {
 
                 // Display it for the driver.
                 opMode.telemetry.addData("Running to",  " %7d :%7d", newlfTarget,  newrfTarget, newlrTarget, newrrTarget);
                 opMode.telemetry.addData("Currently at",  " at %7d :%7d",
-                        initpostsetup.motor_drive_lf.getCurrentPosition(), initpostsetup.motor_drive_rf.getCurrentPosition(), initpostsetup.motor_drive_lr.getCurrentPosition(), initpostsetup.motor_drive_rr.getCurrentPosition(),
-                        initpostsetup.motor_lift.getCurrentPosition(), initpostsetup.motor_swivel.getCurrentPosition(), opMode.telemetry.update());
-            }
+                        initpostsetup.motor_drive_lf.getCurrentPosition(), initpostsetup.motor_drive_rf.getCurrentPosition(), initpostsetup.motor_drive_lr.getCurrentPosition(), initpostsetup.motor_drive_rr.getCurrentPosition());
+
 
             // Stop all motion;
             initpostsetup.motor_drive_lf.setPower(0);
             initpostsetup.motor_drive_rf.setPower(0);
             initpostsetup.motor_drive_lr.setPower(0);
             initpostsetup.motor_drive_rr.setPower(0);
-            initpostsetup.motor_lift.setPower(0);
-            initpostsetup.motor_swivel.setPower(0);
 
             // Turn off RUN_TO_POSITION
             initpostsetup.motor_drive_lf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             initpostsetup.motor_drive_rf.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             initpostsetup.motor_drive_lr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             initpostsetup.motor_drive_rr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            initpostsetup.motor_lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            initpostsetup.motor_swivel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
             opMode.sleep(50);   // optional pause after each move.
         }
     }
-    public void encoderLift(double lftMM, LinearOpMode opMode, InitSetup initpostsetup) {
-        int newlftTarget;
-
-        // Ensure that the opmode is still active
-        if (opMode.opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            //newlftTarget = initpostsetup.motor_lift.getCurrentPosition() + (int) (lftMM * InitSetup.COUNTS_PER_MM_LIFT);
-            newlftTarget = (int)(lftMM * InitSetup.COUNTS_PER_MM_LIFT);
-            initpostsetup.motor_lift.setTargetPosition(newlftTarget);
-
-            // Turn On RUN_TO_POSITION
-            initpostsetup.motor_lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            //initpostsetup.motor_lift.setPower(Math.abs(speed));
-
-            // keep looping while we are still active, and there is time left, and motors are not within the target threshold.
-
-            // Stop all motion;
-            //initpostsetup.motor_lift.setPower(0);
-
-            // Turn off RUN_TO_POSITION
-            //initpostsetup.motor_lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-            //opMode.sleep(50);   // optional pause after each move.
-        }
-    }
-    public void encoderSwivel(double swvDG, LinearOpMode opMode, InitSetup initpostsetup) {
-        int newswvTarget;
-
-        // Ensure that the opmode is still active
-        if (opMode.opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            newswvTarget = (int)(swvDG * InitSetup.COUNTS_PER_DG_SWIVEL);
-
-            initpostsetup.motor_swivel.setTargetPosition(newswvTarget);
-
-            // Turn On RUN_TO_POSITION
-            initpostsetup.motor_swivel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-
-            // keep looping while we are still active, and there is time left, and motors are not within the target threshold.
-
-
-            //opMode.sleep(50);   // optional pause after each move.
-        }
     }
 }
 
